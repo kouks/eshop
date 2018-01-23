5c497fb0e03671ceb0b99618298db47ae5808a52
Pavel Koch <kouks.koch@gmail.com>
2018-01-23 21:08:50 +0000

    cache put to gitignire

--- cache/views/135252902179a3c9c9908fa4010a6f13785b1474.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-
-asd
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- cache/views/f2822e614642e269eaf9cbc2b324a0e1ebe96d4b.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('main'); ?>
-<div>
-    aasdasd <?php echo e($asd); ?>
-
-</div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
