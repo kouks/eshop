13300927c601d174e71fed950e561bf0c7836ba3
unknown <husseinbahdon1@gmail.com>
2018-01-27 17:56:59 +0000

    add a few things on the login page

--- resources/views/auth/_register.blade.php
+++ resources/views/auth/_register.blade.php
@@ -1,3 +1,36 @@
 <div class="register">
     <h2 class="title is-4 has-text-centered">Register</h2>
+
+     <div class="field">
+        <label class="label">full name </label>
+        <div class="control">
+            <input class="input" type="name" placeholder="full name">
+        </div>
+    </div>
+
+     <div class="field">
+        <label class="label">Username</label>
+        <div class="control">
+            <input class="input" type="text" placeholder="Username">
+        </div>
+    </div>
+
+ <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+            <input class="input" type="email" placeholder="email">
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Password</label>
+        <div class="control">
+            <input class="input" type="password" placeholder="Password">
+        </div>
+    </div>
+
+    <hr>
+    <div class="field">
+        <button class="button is-primary">Login</button>
+    </div>
 </div>
