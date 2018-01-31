4421887e23f89d50c647125e4374185ef1e7f1e0
Pavel Koch <kouks.koch@gmail.com>
2018-01-31 13:16:10 +0000

    profile page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,39 +3,45 @@
 @section('content')
 @include('partials.tabs', ['active' => false])
 
-        <main class="section">
-            <div class="container">
-                <div class="columns">
+<main class="section">
+    <div class="container">
+        User data, editable
+    </div>
+</main>
 
-                    <div class="column is-2-tablet">
-                        @include('users.partials.sidebar')
-                    </div>
+<section class="section">
+    <div class="container">
+        <hr>
+    </div>
+</section>
 
-                    <div class="columns is-9-tablet is-offset-1-tablet">
-                        <div class="box level-right">
-<table class="table is-fullwidth is-striped is-hoverable">
-    <thead>
-        <tr>
-            <th>Order ID</th>
-            <th>order date</th>
-            <th>Price</th>
-            <th>Satus</th>
-            <th>invoice</th>
-        </tr>
-    </thead>
-</table>
-</div>
-                    </div>
+<section class="section">
+    <div class="container">
+        <div class="box">
+            <table class="table is-fullwidth is-striped is-hoverable">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Date</th>
+                        <th>Item</th>
+                        <th>Satus</th>
+                        <th>Invoice</th>
+                    </tr>
+                </thead>
 
-
-                </div>
-            </div>
-        </main>
-
-
-
-
-
+                <tbody>
+                    <tr>
+                        <td>123</td>
+                        <td>yesterday</td>
+                        <td>Shirt</td>
+                        <td>Shipped</td>
+                        <td><a href="#">PDF</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
 @stop
 
 
