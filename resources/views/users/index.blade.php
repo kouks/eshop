38c20746cfef9ead12d5591fb7a1fbd84aec4b74
unknown <husseinbahdon1@gmail.com>
2018-01-31 13:39:10 +0000

    merging conflict solved

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,41 +3,6 @@
 @section('content')
 @include('partials.tabs', ['active' => false])
 
-<<<<<<< HEAD
-        <main class="section">
-            <div class="container">
-                <div class="columns">
-
-                    <div class="column is-2-tablet">
-                        @include('users.partials.sidebar')
-                    </div>
-
-                    <div class="columns is-9-tablet is-offset-1-tablet">
-                        <div class="box level-right">
-                            <table class="table is-fullwidth is-striped is-hoverable">
-                                <thead>
-                                    <tr>
-                                        <th>Order ID</th>
-                                        <th>order date</th>
-                                        <th>Price</th>
-                                        <th>Satus</th>
-                                        <th>invoice</th>
-                                    </tr>
-                                </thead>
-                            </table>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-        </main>
-
-
-
-
-
-=======
 <main class="section">
     <div class="container">
         User data, editable
@@ -77,7 +42,7 @@
         </div>
     </div>
 </section>
->>>>>>> 4421887e23f89d50c647125e4374185ef1e7f1e0
+
 @stop
 
 
