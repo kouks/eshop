95c7d17fc8e238eb0489dc5a85226d69e9a3f3f5
Pavel Koch <kouks.koch@gmail.com>
2018-02-01 13:33:49 +0000

    some shop page sketches

---
+++ resources/views/products/_filters.blade.php
@@ -0,0 +1,31 @@
+<aside class="menu">
+    <p class="menu-label">
+        Filters
+    </p>
+
+    <ul class="menu-list">
+        <li>
+            <a href="#">Category</a>
+            <ul>
+                <li><label class="checkbox"><input type="checkbox"> Men</label></li>
+                <li><label class="checkbox"><input type="checkbox"> Women</label></li>
+                <li><label class="checkbox"><input type="checkbox"> Kids</label></li>
+            </ul>
+        </li>
+        <li><a href="#">Price</a></li>
+    </ul>
+
+    <p class="menu-label">
+        Sort By
+    </p>
+
+    <ul class="menu-list">
+        <li><a href="#">Price</a></li>
+        <li><a href="#">Popularity</a></li>
+        <li><a href="#">Newest</a></li>
+    </ul>
+</aside>
+
+<div class="field">
+    <button class="button is-primary">Apply Filters</button>
+</div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,18 +5,25 @@
 
 <main class="section">
     <div class="container">
-        <div class="columns is-multiline">
+        <div class="columns">
             <div class="column is-3-tablet">
-                @include('products._product')
+                @include('products._filters')
             </div>
-            <div class="column is-3-tablet">
-                @include('products._product')
-            </div>
-            <div class="column is-3-tablet">
-                @include('products._product')
-            </div>
-            <div class="column is-3-tablet">
-                @include('products._product')
+            <div class="column is-8-tablet">
+                <div class="columns is-multiline">
+                    <div class="column is-4-tablet">
+                        @include('products._product')
+                    </div>
+                    <div class="column is-4-tablet">
+                        @include('products._product')
+                    </div>
+                    <div class="column is-4-tablet">
+                        @include('products._product')
+                    </div>
+                    <div class="column is-4-tablet">
+                        @include('products._product')
+                    </div>
+                </div>
             </div>
         </div>
     </div>
